7ee66459da9bc44b340ec24a3d8e18f49714f697
jmjmjames <chungjm0711@gmail.com>
2022-11-15 17:05:05 +0900

    feat: BCryptPasswordEncoder 제거

--- user-service/src/main/java/com/takeit/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/takeit/userservice/UserServiceApplication.java
@@ -3,9 +3,7 @@ package com.takeit.userservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -16,8 +14,4 @@ public class UserServiceApplication {
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
 
-	@Bean
-	public BCryptPasswordEncoder bCryptPasswordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
 }
