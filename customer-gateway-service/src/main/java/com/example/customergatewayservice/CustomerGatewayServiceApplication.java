d198b94e3e9bf2432adab7148168ce9348e0647d
jmjmjames <chungjm0711@gmail.com>
2022-12-12 09:08:15 +0900

    chore: customer-gateway-service 프로젝트 세팅

---
+++ customer-gateway-service/src/main/java/com/example/customergatewayservice/CustomerGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.customergatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerGatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerGatewayServiceApplication.class, args);
+    }
+
+}
---
+++ customer-gateway-service/src/test/java/com/example/customergatewayservice/CustomerGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.customergatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerGatewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
